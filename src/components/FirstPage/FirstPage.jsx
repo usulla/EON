54c9dfc818f131554c5ffbaed2874854d44ee02c
Maria <maria_suxareva@mail.ru>
2019-02-05 18:40:40 +0300

    First page added

--- src/components/FirstPage/FirstPage.jsx
+++ src/components/FirstPage/FirstPage.jsx
@@ -8,17 +8,27 @@ import style from './style.scss'
 class FirstPage extends Component {
     render() {
         return (
-        <div className='FirstPage'>
+            <div className='FirstPage'>
         <div className='FirstPage__center-block'>
+        <div className='FirstPage__toptext'>
         <div className='logo'>
-			<img src={logo_pesni} />
+            <img src={logo_pesni} />
         </div>
-
+        <div className='titles'>
+            <div className='title1'>
+            Пройди тест и узнай, как далеко<br/>
+            ты пройдешь <span>в шоу «ПЕСНИ»</span>
+            </div>
+            <div className='title2'>
+            Будь всегда на <img src={logo_eon} /> впереди!
+            </div>
+        </div>
+                </div>
         <div className='bottles_line'>
-        	<div className='bottles'>
-       			 <img src={bottles_eon} alt='EON'/>
-       		 </div>
-       	</div>
+            <div className='bottles'>
+                {/* <img src={bottles_eon} alt='EON'/> */}
+             </div>
+        </div>
         </div>
         </div>
         );
