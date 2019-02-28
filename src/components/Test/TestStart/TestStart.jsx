6ca6552de94153201b2e64b18ae79133744e15fc
Maria <maria_suxareva@mail.ru>
2019-02-28 17:48:58 +0300

    Refactored code TestStart component

--- src/components/Test/TestStart/TestStart.jsx
+++ src/components/Test/TestStart/TestStart.jsx
@@ -1,29 +1,31 @@
-import React, { Component } from "react";
-import style from "./style.scss";
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './style.scss';
 
-class TestStart extends Component {
-    render() {
-        return (
-            <div className="TestStart">
-                <div className="title">
-                    <span>
-                        Пройди тест и узнай,
-                        <br />
-                    </span>
-                    как далеко ты пройдешь
-                    <br />в шоу «ПЕСНИ»
-                </div>
-                <div
-                    className="battery_btn"
-                    onClick={() => {
-                        this.props.updatetest(false, true, false);
-                    }}
-                >
-                    Начать тест
-                </div>
+const TestStart = ({ updatetest = f => f }) => {
+    return (
+        <div className='TestStart'>
+            <div className='title'>
+                <span>
+                    Пройди тест и узнай,
+                    <br />
+                </span>
+                как далеко ты пройдешь
+                <br />в шоу «ПЕСНИ»
             </div>
-        );
-    }
-}
+            <div
+                className='battery_btn'
+                onClick={() => {
+                    updatetest(false, true, false);
+                }}>
+                Начать тест
+            </div>
+        </div>
+    );
+};
+
+TestStart.propTypes = {
+    updatetest: PropTypes.func.isRequired
+};
 
 export default TestStart;
