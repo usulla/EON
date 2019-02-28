e8b2352dd2cbefa6717d101fa83bad0bdf89f383
Maria <maria_suxareva@mail.ru>
2019-02-28 16:59:19 +0300

    Refactored Questions component

--- src/components/Test/Questions/Questions.jsx
+++ src/components/Test/Questions/Questions.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import style from "./style.scss";
-import dataQuestions from "./Questions.json";
-import "nodelist-foreach-polyfill";
+import React, { Component } from 'react';
+import style from './style.scss';
+import dataQuestions from './Questions.json';
+import 'nodelist-foreach-polyfill';
 
 class Questions extends Component {
     constructor() {
@@ -30,16 +30,16 @@ class Questions extends Component {
             ][usersResponse];
 
             // Add classes to answers
-            document.querySelectorAll(".answer").forEach((item, index) => {
+            document.querySelectorAll('.answer').forEach((item, index) => {
                 const itemPoint = this.state.answers[
                     this.state.currentQuestion
                 ][index];
                 if (itemPoint === 2) {
-                    item.classList.add("true");
+                    item.classList.add('true');
                 } else if (itemPoint === 0) {
-                    item.classList.add("null");
+                    item.classList.add('null');
                 } else {
-                    item.classList.add("error");
+                    item.classList.add('error');
                 }
             });
             // Increase currentQuestion or equate to zero if this is the last question
@@ -55,21 +55,21 @@ class Questions extends Component {
                                     : 0
                         };
                     });
-                    document.querySelectorAll(".answer").forEach(item => {
-                        item.classList.remove("null");
-                        item.classList.remove("error");
-                        item.classList.remove("true");
+                    document.querySelectorAll('.answer').forEach(item => {
+                        item.classList.remove('null');
+                        item.classList.remove('error');
+                        item.classList.remove('true');
                     });
                     // Mark the current question in the panel with the question number
                     if (
-                        document.querySelectorAll(".panels_num_quesion__item")[
+                        document.querySelectorAll('.panels_num_quesion__item')[
                             this.state.currentQuestion - 1
                         ]
                     ) {
                         document
-                            .querySelectorAll(".panels_num_quesion__item")
+                            .querySelectorAll('.panels_num_quesion__item')
                             [this.state.currentQuestion - 1].classList.add(
-                                "current"
+                                'current'
                             );
                     } else {
                         if (this.state.userPoints === 0) {
@@ -89,9 +89,9 @@ class Questions extends Component {
                         this.props.updatetest(false, false, true);
 
                         document
-                            .querySelectorAll(".panels_num_quesion__item")
+                            .querySelectorAll('.panels_num_quesion__item')
                             .forEach(item => {
-                                item.classList.remove("current");
+                                item.classList.remove('current');
                             });
                     }
                 }.bind(this),
@@ -108,58 +108,52 @@ class Questions extends Component {
     };
 
     render() {
+        const { currentQuestion } = this.state;
+        const {questions} = dataQuestions; 
         return (
-            <div className="Questions">
-                <div
-                    className={`battery_btn btn${this.state.currentQuestion +
-                        1}`}
-                />
-                <div className="question_title">
-                    {
-                        dataQuestions.questions[this.state.currentQuestion]
-                            .question
-                    }
+            <div className='Questions'>
+                <div className={`battery_btn btn${currentQuestion + 1}`} />
+                <div className='question_title'>
+                    {questions[currentQuestion].question}
                 </div>
 
-                <div className="answers">
-                    {dataQuestions.questions[
-                        this.state.currentQuestion
-                    ].answers.map((item, index) => {
-                        return (
-                            <div
-                                className={`answer overanswer question${this
-                                    .state.currentQuestion + 1}`}
-                                key={index}
-                                data-id={index}
-                                onClick={this.nextQuestion}
-                            >
-                                <div className="answer_content">
-                                    <div className="answer_num">
-                                        {index + 1}.
+                <div className='answers'>
+                    {questions[currentQuestion].answers.map(
+                        (item, index) => {
+                            return (
+                                <div
+                                    className={`answer overanswer question${currentQuestion +
+                                        1}`}
+                                    key={index}
+                                    data-id={index}
+                                    onClick={this.nextQuestion}>
+                                    <div className='answer_content'>
+                                        <div className='answer_num'>
+                                            {index + 1}.
+                                        </div>
+                                        <div className='answer_text'>
+                                            {item}
+                                        </div>
                                     </div>
-                                    <div className="answer_text">{item}</div>
                                 </div>
+                            );
+                        }
+                    )}
+                </div>
+
+                <div className={`panels_num_quesion q${currentQuestion + 1}`}>
+                    {[...Array(3)].map((n, i) => {
+                        return (
+                            <div className='panels_num_quesion__item' key={i}>
+                                <span className='num_answer'>
+                                    вопрос {i+1}
+                                </span>
                             </div>
                         );
                     })}
-                </div>
-
-                <div
-                    className={`panels_num_quesion q${this.state
-                        .currentQuestion + 1}`}
-                >
-                    <div className="panels_num_quesion__item">
-                        <span className="num_answer">вопрос 1</span>
-                    </div>
-                    <div className="panels_num_quesion__item">
-                        <span className="num_answer">вопрос 2</span>
-                    </div>
-                    <div className="panels_num_quesion__item">
-                        <span className="num_answer">вопрос 3</span>
-                    </div>
-                    <div className="panels_num_quesion__item">
-                        <span className="num_answer">вопрос 4</span>
-                        <span className="num_answer">вопрос 5</span>
+                    <div className='panels_num_quesion__item'>
+                        <span className='num_answer'>вопрос 4</span>
+                        <span className='num_answer'>вопрос 5</span>
                     </div>
                 </div>
             </div>
